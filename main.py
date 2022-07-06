72dc0335df994bb404e59e7c0cc92d5da2249a46
Zdravko-Jovancevic <jovancevic95zdravko@gmail.com>
2022-07-06 16:51:37 +0200

    tweaked 500.html

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def user(name):
 def page_not_found(e):
 	return render_template("404.html"), 404
 
-# Internal Server Error
+# Internal Server Error Thing
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500	
